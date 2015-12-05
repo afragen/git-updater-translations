e15f4362802dbfa95e7337a41ff3b6ce74ca8a12
Andy Fragen <andy@thefragens.com>
2015-12-05 12:14:31 -0800

    update plugin headers

--- github-updater-translations.php
+++ github-updater-translations.php
@@ -7,8 +7,10 @@ Version:           0.0.1
 Author:            Andy Fragen
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:       github-updater
 Network:           true
 GitHub Plugin URI: https://github.com/afragen/github-updater-translations
+GitHub Languages:  https://github.com/afragen/github-updater-translations
 GitHub Branch:     master
 Requires WP:       3.8
 Requires PHP:      5.3

b849121ae3cdb917752372a8d284c1404ec5ad0d
Andy Fragen <andy@thefragens.com>
2015-12-05 10:43:33 -0800

    add plugin file

---
+++ github-updater-translations.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name:       GitHub Updater Translations
+Plugin URI:        https://github.com/afragen/github-updater-translations
+Description:       Translation files for the GitHub Updater plugin.
+Version:           0.0.1
+Author:            Andy Fragen
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+Network:           true
+GitHub Plugin URI: https://github.com/afragen/github-updater-translations
+GitHub Branch:     master
+Requires WP:       3.8
+Requires PHP:      5.3
+*/

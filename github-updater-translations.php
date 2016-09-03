2e0a496f7977c27429bee19ea1fb3ac9c17c2d1f
Andy Fragen <andy@thefragens.com>
2016-09-03 11:18:38 -0700

    update plugin file, add load_plugin_textdomain

--- github-updater-translations.php
+++ github-updater-translations.php
@@ -1,17 +1,24 @@
 <?php
-/*
-Plugin Name:       GitHub Updater Translations
-Plugin URI:        https://github.com/afragen/github-updater-translations
-Description:       Translation files for the GitHub Updater plugin.
-Version:           0.0.1
-Author:            Andy Fragen
-License:           GNU General Public License v2
-License URI:       http://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:       github-updater
-Network:           true
-GitHub Plugin URI: https://github.com/afragen/github-updater-translations
-GitHub Languages:  https://github.com/afragen/github-updater-translations
-GitHub Branch:     master
-Requires WP:       3.8
-Requires PHP:      5.3
-*/
+/**
+ * Plugin Name:       GitHub Updater Translations
+ * Plugin URI:        https://github.com/afragen/github-updater-translations
+ * Description:       Translation files for the GitHub Updater plugin.
+ * Version:           0.0.1
+ * Author:            Andy Fragen
+ * License:           GNU General Public License v2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       github-updater
+ * Domain Path:       /languages
+ * Network:           true
+ * GitHub Plugin URI: https://github.com/afragen/github-updater-translations
+ * GitHub Languages:  https://github.com/afragen/github-updater-translations
+ * GitHub Branch:     develop
+ * Requires WP:       3.8
+ * Requires PHP:      5.3
+ */
+
+// Load textdomain
+load_plugin_textdomain( 'github-updater', false, __DIR__ . '/languages' );
+
+// load for localhost using Pressmatic
+//load_plugin_textdomain( 'github-updater', false, basename( __DIR__ ) . '/languages' );

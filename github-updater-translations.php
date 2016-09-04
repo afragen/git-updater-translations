2793243d139506cab76864893d2b765e9dde9289
Andy Fragen <andy@thefragens.com>
2016-09-04 08:53:48 -0700

    call from init hook

--- github-updater-translations.php
+++ github-updater-translations.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GitHub Updater Language Pack
  * Plugin URI:        https://github.com/afragen/github-updater-translations
  * Description:       Translation files for the GitHub Updater plugin.
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Andy Fragen
  * License:           GNU General Public License v2
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
@@ -15,8 +15,7 @@
  * Requires PHP:      5.3
  */
 
-// Load textdomain
-load_plugin_textdomain( 'github-updater', false, __DIR__ . '/languages' );
 
-// load for localhost using Pressmatic
-//load_plugin_textdomain( 'github-updater', false, basename( __DIR__ ) . '/languages' );
+add_action( 'init', function() {
+	load_plugin_textdomain( 'github-updater', false, basename( __DIR__ ) . '/languages' );
+} );
